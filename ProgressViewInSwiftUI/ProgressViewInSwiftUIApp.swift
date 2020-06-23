f50e26a36d5c2f7858deaae6728546908ced67a3
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2020-06-23 07:53:07 +0300

    Initial Commit

---
+++ ProgressViewInSwiftUI/ProgressViewInSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProgressViewInSwiftUIApp.swift
+//  ProgressViewInSwiftUI
+//
+//  Created by Ramill Ibragimov on 23.06.2020.
+//
+
+import SwiftUI
+
+@main
+struct ProgressViewInSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
