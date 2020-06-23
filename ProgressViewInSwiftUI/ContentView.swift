c3636c18d6f78ee370bfbab9b8b26b83048e5209
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2020-06-23 08:03:59 +0300

    init

--- ProgressViewInSwiftUI/ContentView.swift
+++ ProgressViewInSwiftUI/ContentView.swift
@@ -8,8 +8,29 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var downloaded: CGFloat = 0
+    
     var body: some View {
-        Text("Hello, world!").padding()
+        VStack {
+            VStack {
+                ProgressView("Loading", value: self.downloaded, total: 100)
+                
+                Button("Download") {
+                    self.startDownload()
+                }
+            }.padding(.all, 10)
+        }
+    }
+    
+    private func startDownload() {
+        let timer = Timer(timeInterval: 0.1, repeats: true) { (timer) in
+            if self.downloaded < 100 {
+                self.downloaded += 1
+            } else {
+                timer.invalidate()
+            }
+        }
+        RunLoop.main.add(timer, forMode: .common)
     }
 }
 
